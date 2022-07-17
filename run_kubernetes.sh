69a60c934260a36585dc52c88f1ae0d7772c3f05
Abraham James <abraham.o.james442@gmail.com>
2022-07-17 15:58:25 +0100

    Update all .sh files

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,16 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=horllymobile/horlamidex_ml_project
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run horlamidex_ml_project --image=$dockerpath --generator=run-pod/v1
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward horlamidex_ml_project 8000:80
